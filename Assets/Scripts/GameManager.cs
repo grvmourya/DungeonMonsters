93686de2072311b9dcf262638ed9b6539285369c
grvmourya <gauravmourya1999@gmail.com>
2022-01-20 15:12:16 +0530

    dated 20 Jan

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+    private void Awake()
     {
-        
+        if(GameManager.instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        SceneManager.sceneLoaded += LoadState;
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    //resources
+    public List<Sprite> playerSprites;
+    public List<Sprite> weaponSprites;
+    public List<int> weaponPrices;
+    public List<int> xpTable;
+
+    //References
+    public Player player;
+    //public Weapon weapon...
+
+    //Logic
+    public int pesos;
+    public int experience;
+
+    //save state
+    /*
+     * int preferedSkin
+     * int pesos
+     * int experiences
+     * int weaponLevel
+     */
+
+
+    public void SaveState()
     {
-        
+        string s = "";
+
+        s += "0" + "|";
+        s += pesos.ToString() + "|";
+        s += experience.ToString() + "|";
+        s += "0";
+
+        PlayerPrefs.SetString("SaveState", s); //a key and a value
     }
+
+    public void LoadState(Scene s, LoadSceneMode mode)
+    {
+        if (!PlayerPrefs.HasKey("SaveState"))
+            return;
+
+        string[] data = PlayerPrefs.GetString("SaveState").Split('|');
+        pesos = int.Parse(data[1]);
+        experience = int.Parse(data[2]);
+        Debug.Log("Load State");
+    }
+
 }
